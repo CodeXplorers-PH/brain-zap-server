06a8161afe0e0f0f39d3e23b1e670e54914e30b6
s-h-rumon <shahidhasanruman3@gmil.com>
2025-04-10 17:57:13 +0600

    [Updated]: dynamic quiz number added for quiz generating

--- controllers/get/getQuizzes.js
+++ controllers/get/getQuizzes.js
@@ -1,23 +1,20 @@
 const { tryCatch } = require('../../utils/tryCatch');
 const { quizModel } = require('../../config/geminiModel');
 
-// Quizzes Number
-const quizzesNumber = 4;
-
 // Prompt generator
-const generatePrompt = (topic, difficulty = 'easy') => {
+const generatePrompt = (topic, difficulty = 'easy', quizzesNumber = 10) => {
   return `Generate unique and different ${quizzesNumber} quizzes questions about ${topic} with a ${difficulty} difficulty level.`;
 };
 
 // Generate Quiz
 const generateQuiz = tryCatch(async (req, res) => {
-  const { topic, difficulty } = req.query;
+  const { topic, difficulty, quizzesNumber } = req.query;
 
   if (!topic) {
     return res.status(400).json({ error: 'Topic is required!' }); // Return if Topic or Difficulty is missing
   }
 
-  const prompt = generatePrompt(topic, difficulty); // Generate Prompt
+  const prompt = generatePrompt(topic, difficulty, quizzesNumber); // Generate Prompt
   const result = await quizModel.generateContent(prompt); // Gemini response
 
   if (result.error) {
