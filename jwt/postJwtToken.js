2acb92e16c6acd641934a8d057d4433bd85c8fd6
s-h-rumon <shahidhasanruman3@gmil.com>
2025-04-29 13:03:41 +0600

    Updated: jwt token format update & error handled

--- jwt/postJwtToken.js
+++ jwt/postJwtToken.js
@@ -3,11 +3,15 @@ const jwt = require('jsonwebtoken');
 const postJwtToken = (req, res) => {
   const { email } = req.body;
 
+  if (!email) {
+    return res.status(400).send({ message: 'Email is required!' });
+  }
+
   const token = jwt.sign({ email }, process.env.JWT_SECRET, {
     expiresIn: '31d',
   });
 
-  res.send({ token: `Bearer ${token}` });
+  res.send({ token });
 };
 
 module.exports = { postJwtToken };
