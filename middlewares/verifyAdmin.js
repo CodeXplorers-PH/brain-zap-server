da4f690d814c7edccb282ef8d7f254c3877d46b9
s-h-rumon <shahidhasanruman3@gmil.com>
2025-04-30 17:04:15 +0600

    [Updated]: verifyAdmin email changed to headers

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,21 +1,21 @@
-const { connectDB } = require("../config/database");
+const { connectDB } = require('../config/database');
 
 const verifyAdmin = async (req, res, next) => {
   try {
-    const { email } = req.params;
+    const { email } = req.headers;
 
-    const users = await connectDB("users");
+    const users = await connectDB('users');
     const userInfo = await users.findOne({ email });
 
-    const isAdmin = userInfo?.role === "admin";
+    const isAdmin = userInfo?.role === 'admin';
     if (!isAdmin) {
-      return res.status(403).send({ message: "Forbidden Access" });
+      return res.status(401).send({ message: 'Unauthorized Access' });
     }
 
     next();
   } catch (error) {
-    console.error("Error in verifyAdmin middleware:", error);
-    res.status(500).send({ message: "Internal Server Error" });
+    console.error('Error in verifyAdmin middleware:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
   }
 };
 
