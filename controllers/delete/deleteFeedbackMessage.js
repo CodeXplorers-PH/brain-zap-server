e2ab82612e38dee5c0bac7f68010305700892cb3
s-h-rumon <shahidhasanruman3@gmil.com>
2025-05-04 11:51:28 +0600

    Updated: Status changed to 400 on incorrect params

--- controllers/delete/deleteFeedbackMessage.js
+++ controllers/delete/deleteFeedbackMessage.js
@@ -1,18 +1,18 @@
-const { tryCatch } = require("../../utils/tryCatch");
-const { connectDB } = require("../../config/database");
-const { ObjectId } = require("mongodb");
+const { tryCatch } = require('../../utils/tryCatch');
+const { connectDB } = require('../../config/database');
+const { ObjectId } = require('mongodb');
 
 const deleteFeedbackMessage = tryCatch(async (req, res) => {
   const { id } = req.params;
 
-  const feedbackCollection = await connectDB("feedback");
+  const feedbackCollection = await connectDB('feedback');
 
   const result = await feedbackCollection.deleteOne({ _id: new ObjectId(id) });
 
   if (result.deletedCount === 1) {
     res.status(200).json({ success: true });
   } else {
-    res.status(404).json({ success: false, message: "Feedback not found." });
+    res.status(400).json({ success: false, message: "Feedback don't exists." });
   }
 });
 
